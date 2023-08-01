c913259a566abf2d6dcc2c84adf2f95a4302126b
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-01 19:33:19 -0400

    Add index action

--- test/controllers/photos_controller_test.rb
+++ test/controllers/photos_controller_test.rb
@@ -4,4 +4,11 @@ class PhotosControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "index" do
+    get "/photos.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Photo.count, data.length
+  end
 end
