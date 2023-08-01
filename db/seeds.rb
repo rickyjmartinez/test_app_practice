19fadde327083d10179dfb0510d3bc72573deaa7
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-01 19:22:20 -0400

    Add model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+photo = Photo.new(name: "Forest", width: 1000, height: 750)
+photo.save
